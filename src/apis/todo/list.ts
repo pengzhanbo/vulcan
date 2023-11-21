a505852d9ca020c4b091eda1ae24a178e6a05077
pengzhanbo <volodymyr@foxmail.com>
2023-11-21 08:35:50 +0800

    build: update deps to latest

--- src/apis/todo/list.ts
+++ src/apis/todo/list.ts
@@ -3,7 +3,7 @@ import { request } from '~/common/request'
 export interface GetTodoListResponse {
   code: number
   message: string
-  result: { id: number; name: string }[]
+  result: { id: number, name: string }[]
 }
 
 export const fetchTodoList = request.get<never, GetTodoListResponse>('/todo/list')
